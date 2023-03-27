c257ff93434ddefccd6fafc475e69fae84eef64c
Min_Thu32 <minthu@my.jcu.edu.au>
2023-03-27 14:46:52 +0800

    Random numbers using python console

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+"""
+line 1
+smallest number = 8
+largest number = 19
+
+line 2
+smallest number = 3
+largest number = 9
+No, it couldn't produce a 4 since the starting number is 3
+with a step size of 2 which skips 4 when running the code
+
+line 3
+smallest number = 2.5195372505578173
+largest number = 4.936137881256551
+"""
+import random
+
+print(random.randint(1, 100))
