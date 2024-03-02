e8c4a5522bfb7e37faa34ced2ba15f1052204337
Min_Thu32 <minthu@my.jcu.edu.au>
2024-03-03 07:26:47 +0800

    Explain random number generation

---
+++ prac_03/randoms.py
@@ -0,0 +1,20 @@
+"""
+line 1
+Line 1 prints random integer between 5 and 20.
+smallest number = 5
+largest number = 20
+
+line 2
+smallest number = 3
+largest number = 9
+No, it couldn't produce a 4 since the starting number is 3
+with a step size of 2 which skips 4 when running the code
+
+line 3
+Line 3 prints random float number between 2.5 (inclusive) and 5.5 (exclusive).
+smallest number = 2.5
+largest number = 5.99999...
+"""
+import random
+
+print(random.randint(1, 100))
